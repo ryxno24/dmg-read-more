09d891a326ad967ada89e23980787ac1ef9f44e2
Ryan Noon <ryannoon24@gmail.com>
2025-06-28 14:18:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# DMG Read More WordPress Plugin
+
+A WordPress plugin that provides a Gutenberg block for creating stylised "Read More" links to other posts, plus a WP-CLI command for finding posts that use the block.
+
+## Features
+
+### Gutenberg Block
+
+
+### WP-CLI Command
+
+
+## Installation
+
+1. Upload the plugin folder to `/wp-content/plugins/`
+2. Activate the plugin through the WordPress admin
+3. The "DMG Read More" block will be available in the Gutenberg editor
+
+## Requirements
+
+- WordPress 5.0+
+- PHP 7.4+
+- WP-CLI (for command functionality)
---
+++ dmg-read-more.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: DMG Read More
+ * Description: A WordPress plugin with a Gutenberg block for linking to posts and a WP-CLI command for searching block usage.
+ * Version: 1.0.0
+ * Author: Ryan Noon
+ * Text Domain: dmg-read-more
+ * Requires at least: 5.0
+ * Tested up to: 6.4
+ * Requires PHP: 7.4
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+define('DMG_READ_MORE_VERSION', '1.0.0');
+define('DMG_READ_MORE_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('DMG_READ_MORE_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+class DMG_Read_More {
+    
+    public function __construct() {
+        add_action('init', array($this, 'init'));
+    }
+    
+    public function init() {
+        
+    }
+}
+
+new DMG_Read_More();
